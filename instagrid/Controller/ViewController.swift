737c35ed392b7dfc979e2f279731a5e12a807a74
zouritre <timouf97400@live.fr>
2022-02-20 02:59:19 +0100

    WIP - Setting up UI. Portrait mode is done for iPhone11

--- instagrid/Controller/ViewController.swift
+++ instagrid/Controller/ViewController.swift
@@ -14,26 +14,76 @@ class ViewController: UIViewController {
     @IBOutlet weak var instagridLabel: UILabel!
     @IBOutlet weak var swipeToShare: UILabel!
     
-    @IBOutlet weak var dispo1: UIView!
-    @IBOutlet weak var dispo2: UIView!
-    @IBOutlet weak var dispo3: UIView!
+    @IBOutlet weak var disposition1: UIStackView!
+    @IBOutlet weak var disposition2: UIStackView!
+    @IBOutlet weak var disposition3: UIStackView!
+    
+    @IBOutlet weak var showDispo1: UIImageView!
+    @IBOutlet weak var showDispo2: UIImageView!
+    @IBOutlet weak var showDispo3: UIImageView!
+    
+    private var currentlySelectedDisposition : UIStackView!
+    
+    
+    private func setTapRecognizerOnDispositionImages() {
+        
+
+        let dispositionImageViews : [UIImageView] = [showDispo1!, showDispo2!, showDispo3!]
+
+        for element in dispositionImageViews {
+            
+            let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer:)))
+            
+            element.isUserInteractionEnabled = true
+            element.addGestureRecognizer(tapGestureRecognizer)
+
+        }
+        
+        currentlySelectedDisposition = disposition1
+
+    }
+    
+    @objc func imageTapped(tapGestureRecognizer: UITapGestureRecognizer)
+    {
+        
+        if let previousDisposition = currentlySelectedDisposition {
+
+
+            previousDisposition.isHidden = true
+
+            let tappedImage = tapGestureRecognizer.view as! UIImageView
+
+            switch tappedImage.tag{
+
+            case 0: disposition1.isHidden = false; currentlySelectedDisposition = disposition1
+            case 1: disposition2.isHidden = false; currentlySelectedDisposition = disposition2
+            case 2: disposition3.isHidden = false; currentlySelectedDisposition = disposition3
+
+            default: print("Couldn't load selected disposition")
+
+            }
+
+        }
+
+        else {
+
+            print("currentlySelectedDisposition var is nil")
+
+        }
+
+    }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         instagridLabel.font = UIFont(name: "Thirsty soft", size: 45)
+        
         swipeToShare.font = UIFont(name: "Delm", size: 26)
         
+        setTapRecognizerOnDispositionImages()
+
         
-//        for family: String in UIFont.familyNames
-//        {
-//            print(family)
-//            for names: String in UIFont.fontNames(forFamilyName: family)
-//            {
-//                print("== \(names)")
-//            }
-//        }
     }
 
 
